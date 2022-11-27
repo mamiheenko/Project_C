f94dadd9d6f3ebfbb17599376d385292f82708a8
Maksim <mamiheenko@gmail.com>
2022-11-27 20:18:56 +0300

    add lesson_5

---
+++ lesson_5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя: ");
+string username=Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+Console.WriteLine("Ураа, это же МАША!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
